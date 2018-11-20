18f5fc4dba12a46d6cc0311fbed1904d9573add1
aslan <aslan@vivalnk.com.cn>
2018-11-20 10:17:13 +0800

    增加跳转

--- baselibrary/src/main/java/com/aslan/baselibrary/listener/IBaseView.java
+++ baselibrary/src/main/java/com/aslan/baselibrary/listener/IBaseView.java
@@ -1,6 +1,10 @@
 package com.aslan.baselibrary.listener;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresPermission;
 import android.support.annotation.StringRes;
 import android.support.annotation.UiThread;
 
@@ -45,4 +49,13 @@ public interface IBaseView {
   void showToastMessage(@NonNull BaseHttpError error);
 
   boolean isAdd();
+
+  @UiThread
+  void startActivity(Intent intent);
+
+  @UiThread
+  void startActivityForResult(@RequiresPermission Intent intent, int requestCode);
+
+  @UiThread
+  void startActivityForResult(@RequiresPermission Intent intent, int requestCode, @Nullable Bundle options);
 }
