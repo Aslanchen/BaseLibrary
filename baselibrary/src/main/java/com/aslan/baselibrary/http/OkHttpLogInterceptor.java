aa55a8e5067f374b57733fc0a979f8455bbc38b6
Aslanchen <122560007@163.com>
2018-12-27 21:18:05 +0800

    http日志完善

--- baselibrary/src/main/java/com/aslan/baselibrary/http/OkHttpLogInterceptor.java
+++ baselibrary/src/main/java/com/aslan/baselibrary/http/OkHttpLogInterceptor.java
@@ -2,9 +2,13 @@ package com.aslan.baselibrary.http;
 
 import android.support.annotation.NonNull;
 import android.util.Log;
+
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.UnsupportedCharsetException;
+
 import okhttp3.Interceptor;
+import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
@@ -18,85 +22,104 @@ import okio.BufferedSource;
  */
 public class OkHttpLogInterceptor implements Interceptor {
 
-  public static final String TAG = "HTTP";
+    public static final String TAG = "HTTP";
 
-  private static final Charset UTF8 = Charset.forName("UTF-8");
+    private static final Charset UTF8 = Charset.forName("UTF-8");
 
-  @Override
-  public Response intercept(@NonNull Chain chain) throws IOException {
-    Request request = chain.request();
-    StringBuilder sb = new StringBuilder();
-    sb.append(" ");
-    sb.append(System.getProperty("line.separator"));
-    sb.append("--------------------请求开始-------------------------");
-    sb.append(System.getProperty("line.separator"));
-    sb.append(String.format("Heads：%s", request.headers().toString()));
-    sb.append(System.getProperty("line.separator"));
-    sb.append(String.format("Url：%s", request.url()));
-    sb.append(System.getProperty("line.separator"));
-    sb.append(String.format("Method：%s", request.method()));
-    sb.append(System.getProperty("line.separator"));
-
-    RequestBody requestBody = request.body();
-    if (requestBody != null) {
-      Buffer buffer = new Buffer();
-      requestBody.writeTo(buffer);
-      sb.append(String.format("Body：%s", buffer.readString(UTF8)));
-      sb.append(System.getProperty("line.separator"));
-    }
-    sb.append("--------------------请求结束-------------------------");
-    sb.append(System.getProperty("line.separator"));
-    Log.e(TAG, sb.toString());
-
-    sb.delete(0, sb.length());
-
-    sb.append(" ");
-    sb.append(System.getProperty("line.separator"));
-    sb.append("--------------------回复开始-------------------------");
-    sb.append(System.getProperty("line.separator"));
-
-    Response response = null;
-    try {
-      response = chain.proceed(request);
-    } catch (Exception e) {
-      sb.append(e.getMessage());
-      sb.append(System.getProperty("line.separator"));
-      sb.append("--------------------回复结束-------------------------");
-      sb.append(System.getProperty("line.separator"));
-      Log.e(TAG, sb.toString());
-      throw e;
-    }
-
-    sb.append(System.getProperty("line.separator"));
-    sb.append(String.format("Heads：%s", String.valueOf(response.headers())));
-    sb.append(System.getProperty("line.separator"));
-    sb.append(String.format("Url：%s", response.request().url()));
-    sb.append(System.getProperty("line.separator"));
-    sb.append(String.format("Code：%s", response.code()));
-    sb.append(System.getProperty("line.separator"));
-    sb.append(String.format("Method：%s", response.request().method()));
-    sb.append(System.getProperty("line.separator"));
-
-    if (response.isSuccessful()) {
-
-    } else {
-      sb.append(String.format("Message：%s", response.message()));
-      sb.append(System.getProperty("line.separator"));
-    }
-
-    ResponseBody responseBody = response.body();
-    if (HttpHeaders.hasBody(response)) {
-      if (responseBody != null) {
-        BufferedSource source = responseBody.source();
-        source.request(Long.MAX_VALUE);
-        Buffer buffer = source.buffer();
-        sb.append(String.format("Body：%s", buffer.clone().readString(UTF8)));
+    @Override
+    public Response intercept(@NonNull Chain chain) throws IOException {
+        Request request = chain.request();
+        StringBuilder sb = new StringBuilder();
+        sb.append(" ");
         sb.append(System.getProperty("line.separator"));
-      }
+        sb.append("--------------------请求开始-------------------------");
+        sb.append(System.getProperty("line.separator"));
+        sb.append(String.format("Heads：%s", request.headers().toString()));
+        sb.append(System.getProperty("line.separator"));
+        sb.append(String.format("Url：%s", request.url()));
+        sb.append(System.getProperty("line.separator"));
+        sb.append(String.format("Method：%s", request.method()));
+        sb.append(System.getProperty("line.separator"));
+
+        RequestBody requestBody = request.body();
+        if (requestBody != null) {
+            Buffer buffer = new Buffer();
+            requestBody.writeTo(buffer);
+
+            Charset charset = UTF8;
+            MediaType contentType = requestBody.contentType();
+            if (contentType != null) {
+                charset = contentType.charset(UTF8);
+            }
+            sb.append(String.format("Body：%s", buffer.readString(charset)));
+            sb.append(System.getProperty("line.separator"));
+        }
+        sb.append("--------------------请求结束-------------------------");
+        sb.append(System.getProperty("line.separator"));
+        Log.e(TAG, sb.toString());
+
+        sb.delete(0, sb.length());
+
+        sb.append(" ");
+        sb.append(System.getProperty("line.separator"));
+        sb.append("--------------------回复开始-------------------------");
+        sb.append(System.getProperty("line.separator"));
+
+        Response response = null;
+        try {
+            response = chain.proceed(request);
+        } catch (Exception e) {
+            sb.append(e.getMessage());
+            sb.append(System.getProperty("line.separator"));
+            sb.append("--------------------回复结束-------------------------");
+            sb.append(System.getProperty("line.separator"));
+            Log.e(TAG, sb.toString());
+            throw e;
+        }
+
+        sb.append(System.getProperty("line.separator"));
+        sb.append(String.format("Heads：%s", String.valueOf(response.headers())));
+        sb.append(System.getProperty("line.separator"));
+        sb.append(String.format("Url：%s", response.request().url()));
+        sb.append(System.getProperty("line.separator"));
+        sb.append(String.format("Code：%s", response.code()));
+        sb.append(System.getProperty("line.separator"));
+        sb.append(String.format("Method：%s", response.request().method()));
+        sb.append(System.getProperty("line.separator"));
+
+        if (response.isSuccessful()) {
+
+        } else {
+            sb.append(String.format("Message：%s", response.message()));
+            sb.append(System.getProperty("line.separator"));
+        }
+
+        ResponseBody responseBody = response.body();
+        if (HttpHeaders.hasBody(response)) {
+            if (responseBody != null) {
+                BufferedSource source = responseBody.source();
+                source.request(Long.MAX_VALUE);
+                Buffer buffer = source.getBuffer();
+
+                Charset charset = UTF8;
+                MediaType contentType = responseBody.contentType();
+                if (contentType != null) {
+                    try {
+                        charset = contentType.charset(UTF8);
+                    } catch (UnsupportedCharsetException e) {
+                        sb.append("--------------------回复结束-------------------------");
+                        sb.append(System.getProperty("line.separator"));
+                        return response;
+                    }
+                }
+
+                sb.append(String.format("Body：%s", buffer.clone().readString(charset)));
+                sb.append(System.getProperty("line.separator"));
+            }
+        }
+        sb.append("--------------------回复结束-------------------------");
+        sb.append(System.getProperty("line.separator"));
+        Log.e(TAG, sb.toString());
+        return response;
     }
-    sb.append("--------------------回复结束-------------------------");
-    sb.append(System.getProperty("line.separator"));
-    Log.e(TAG, sb.toString());
-    return response;
-  }
 }
--- baselibrary/src/main/java/com/aslan/baselibrary/http/callback/BaseCallback.java
+++ baselibrary/src/main/java/com/aslan/baselibrary/http/callback/BaseCallback.java
@@ -4,9 +4,12 @@ import android.arch.lifecycle.Lifecycle;
 import android.arch.lifecycle.LifecycleOwner;
 import android.content.Context;
 import android.support.annotation.NonNull;
+
 import com.aslan.baselibrary.http.BaseHttpError;
 import com.google.gson.Gson;
+
 import java.io.IOException;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
@@ -17,70 +20,70 @@ import okhttp3.ResponseBody;
  */
 public abstract class BaseCallback<T> implements Callback {
 
-  protected static Gson gson = new Gson();
+    protected static Gson gson = new Gson();
 
-  public LifecycleOwner lifecycleOwner;
-  public Context context;
+    public LifecycleOwner lifecycleOwner;
+    public Context context;
 
-  public BaseCallback(Context context, LifecycleOwner lifecycleOwner) {
-    this.context = context;
-    this.lifecycleOwner = lifecycleOwner;
-  }
-
-  @Override
-  public void onFailure(@NonNull Call call, @NonNull IOException e) {
-    if (lifecycleOwner != null
-        && lifecycleOwner.getLifecycle().getCurrentState() == Lifecycle.State.DESTROYED) {
-      onFinish();
-      return;
+    public BaseCallback(Context context, LifecycleOwner lifecycleOwner) {
+        this.context = context;
+        this.lifecycleOwner = lifecycleOwner;
     }
 
-    if (call.isCanceled()) {
-      onFinish();
-      return;
+    @Override
+    public void onFailure(@NonNull Call call, @NonNull IOException e) {
+        if (lifecycleOwner != null
+                && lifecycleOwner.getLifecycle().getCurrentState() == Lifecycle.State.DESTROYED) {
+            onFinish();
+            return;
+        }
+
+        if (call.isCanceled()) {
+            onFinish();
+            return;
+        }
+
+        onFailure(new BaseHttpError(context, e));
+        onFinish();
     }
 
-    onFailure(new BaseHttpError(context, e));
-    onFinish();
-  }
+    @Override
+    public void onResponse(@NonNull Call call, @NonNull Response response) {
+        if (lifecycleOwner != null
+                && lifecycleOwner.getLifecycle().getCurrentState() == Lifecycle.State.DESTROYED) {
+            onFinish();
+            return;
+        }
 
-  @Override
-  public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-    if (lifecycleOwner != null
-        && lifecycleOwner.getLifecycle().getCurrentState() == Lifecycle.State.DESTROYED) {
-      onFinish();
-      return;
+        if (call.isCanceled()) {
+            onFinish();
+            return;
+        }
+
+        ResponseBody responseBody = response.body();
+        if (responseBody == null) {
+            onFailure(new BaseHttpError(context));
+            return;
+        }
+
+        try {
+            T data = handleResponse(response);
+            if (data != null) {
+                onResponse(data);
+            }
+        } catch (Exception e) {
+            onFailure(new BaseHttpError(context));
+        }
+        onFinish();
     }
 
-    if (call.isCanceled()) {
-      onFinish();
-      return;
+    public void onFinish() {
+
     }
 
-    ResponseBody responseBody = response.body();
-    if (responseBody == null) {
-      onFailure(new BaseHttpError(context));
-      return;
-    }
+    public abstract void onFailure(BaseHttpError baseHttpError);
 
-    try {
-      T data = handleResponse(response);
-      if (data != null) {
-        onResponse(data);
-      }
-    } catch (Exception e) {
-      onFailure(new BaseHttpError(context));
-    }
-    onFinish();
-  }
+    public abstract T handleResponse(@NonNull Response response) throws IOException;
 
-  public void onFinish() {
-
-  }
-
-  public abstract void onFailure(BaseHttpError baseHttpError);
-
-  public abstract T handleResponse(@NonNull Response response) throws IOException;
-
-  public abstract void onResponse(T respone);
+    public abstract void onResponse(T respone);
 }
