28394abef7f2a76c71a65731424a24184b471974
aslan <aslan@vivalnk.com.cn>
2019-03-12 10:25:06 +0800

    通知栏优化

--- baselibrary/src/main/java/com/aslan/baselibrary/utils/NotificationHelp.java
+++ baselibrary/src/main/java/com/aslan/baselibrary/utils/NotificationHelp.java
@@ -7,12 +7,14 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Build.VERSION_CODES;
 import android.provider.Settings;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.NotificationCompat;
@@ -36,16 +38,8 @@ public final class NotificationHelp {
     this.context = context;
   }
 
-  public NotificationCompat.Builder creat(@DrawableRes int icon, @NonNull String channelId,
-      @NonNull String channelName) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      createNotificationChannel(context, channelId, channelName,
-          NotificationManagerCompat.IMPORTANCE_DEFAULT);
-    }
-    mNotificationManager = NotificationManagerCompat.from(context);
-    builder = new NotificationCompat.Builder(context, channelId);
-    builder.setSmallIcon(icon);
-    return builder;
+  public NotificationCompat.Builder creatBuilder(@DrawableRes int icon, @NonNull String channelId) {
+    return creatBuilder(icon, channelId, NotificationManagerCompat.IMPORTANCE_DEFAULT);
   }
 
   /**
@@ -58,11 +52,8 @@ public final class NotificationHelp {
    * IMPORTANCE_DEFAULT 开启通知，不会弹出，发出提示音，状态栏中显示</p>
    * IMPORTANCE_HIGH 开启通知，会弹出，发出提示音，状态栏中显示</p>
    */
-  public NotificationCompat.Builder creat(@DrawableRes int icon, @NonNull String channelId,
-      @NonNull String channelName, int importance) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      createNotificationChannel(context, channelId, channelName, importance);
-    }
+  public NotificationCompat.Builder creatBuilder(@DrawableRes int icon, @NonNull String channelId,
+      int importance) {
     mNotificationManager = NotificationManagerCompat.from(context);
     builder = new NotificationCompat.Builder(context, channelId);
     if (importance == NotificationManagerCompat.IMPORTANCE_NONE) {
@@ -82,13 +73,28 @@ public final class NotificationHelp {
     return builder;
   }
 
+  /**
+   * @param importance {@link NotificationManagerCompat#IMPORTANCE_DEFAULT}
+   */
   @RequiresApi(api = VERSION_CODES.O)
-  private void createNotificationChannel(Context context, @NonNull String channelId,
+  public static void createNotificationChannel(Context context, @NonNull String channelId,
       @NonNull String channelName, int importance) {
-    NotificationChannel chan = new NotificationChannel(channelId, channelName, importance);
+    createNotificationChannel(context, channelId, channelName, importance, null);
+  }
+
+  /**
+   * @param importance {@link NotificationManagerCompat#IMPORTANCE_DEFAULT}
+   */
+  @RequiresApi(api = VERSION_CODES.O)
+  public static void createNotificationChannel(Context context, @NonNull String channelId,
+      @NonNull String channelName, int importance, @Nullable IChannelSetting iChannelSetting) {
+    NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
     NotificationManager notificationManager = (NotificationManager) context
         .getSystemService(Context.NOTIFICATION_SERVICE);
-    notificationManager.createNotificationChannel(chan);
+    if (iChannelSetting != null) {
+      iChannelSetting.onChannelSetting(channel);
+    }
+    notificationManager.createNotificationChannel(channel);
   }
 
   public Notification build() {
@@ -121,6 +127,17 @@ public final class NotificationHelp {
   }
 
   /**
+   * 通知中某个channel是否开启
+   */
+  @RequiresApi(api = VERSION_CODES.O)
+  public static boolean isChannelEnabled(Context context, String channelId) {
+    NotificationManager manager = (NotificationManager) context
+        .getSystemService(Context.NOTIFICATION_SERVICE);
+    NotificationChannel channel = manager.getNotificationChannel(channelId);
+    return channel.getImportance() != NotificationManager.IMPORTANCE_NONE;
+  }
+
+  /**
    * 通知是否可用，针对4.4以上的系统才有效，4.4以下返回true
    */
   public static boolean isEnabled(Context context) {
@@ -129,52 +146,81 @@ public final class NotificationHelp {
   }
 
   /**
-   * 设置通知开关
+   * 打开channel设置界面
    */
-  public static void gotoNotificationSetting(Activity activity) {
-    try {
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        //这种方案适用于 API 26, 即8.0（含8.0）以上可以用
-        ApplicationInfo appInfo = activity.getApplicationInfo();
-        String pkg = activity.getApplicationContext().getPackageName();
-        int uid = appInfo.uid;
-        Intent intent = new Intent();
-        intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
-        intent.putExtra(Settings.EXTRA_APP_PACKAGE, pkg);
-        intent.putExtra(Settings.EXTRA_CHANNEL_ID, uid);
-        activity.startActivityForResult(intent, 100);
-      } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-        //这种方案适用于 API21——25，即 5.0——7.1 之间的版本可以使用
-        ApplicationInfo appInfo = activity.getApplicationInfo();
-        String pkg = activity.getApplicationContext().getPackageName();
-        int uid = appInfo.uid;
-        Intent intent = new Intent();
-        intent.setAction("android.settings.APP_NOTIFICATION_SETTINGS");
-        intent.putExtra("app_package", pkg);
-        intent.putExtra("app_uid", uid);
-        activity.startActivityForResult(intent, 100);
-      } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT) {
-        ApplicationInfo appInfo = activity.getApplicationInfo();
-        String pkg = activity.getApplicationContext().getPackageName();
-        Intent intent = new Intent();
-        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        intent.addCategory(Intent.CATEGORY_DEFAULT);
-        intent.setData(Uri.parse("package:" + pkg));
-        activity.startActivityForResult(intent, 100);
-      } else {
-        Intent intent = new Intent(Settings.ACTION_SETTINGS);
-        activity.startActivityForResult(intent, 100);
-      }
-    } catch (Exception e) {
-      Intent intent = new Intent(Settings.ACTION_SETTINGS);
-      activity.startActivityForResult(intent, 100);
+  @RequiresApi(api = VERSION_CODES.O)
+  public static void openChannelSetting(Activity activity, String channelId, int requestCode) {
+    Intent intent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS);
+    intent.putExtra(Settings.EXTRA_APP_PACKAGE, activity.getPackageName());
+    intent.putExtra(Settings.EXTRA_CHANNEL_ID, channelId);
+    if (activity.getPackageManager().resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY)
+        != null) {
+      activity.startActivityForResult(intent, requestCode);
+    }
+  }
+
+  /**
+   * 打开channel设置界面
+   */
+  @RequiresApi(api = VERSION_CODES.O)
+  public static void openChannelSetting(Fragment fragment, String channelId, int requestCode) {
+    Intent intent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS);
+    intent.putExtra(Settings.EXTRA_APP_PACKAGE, fragment.getContext().getPackageName());
+    intent.putExtra(Settings.EXTRA_CHANNEL_ID, channelId);
+    if (fragment.getContext().getPackageManager()
+        .resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY)
+        != null) {
+      fragment.startActivityForResult(intent, requestCode);
     }
   }
 
   /**
    * 设置通知开关
    */
-  public static void gotoNotificationSetting(Fragment fragment) {
+  public static void openNotificationSetting(Activity activity, int requestCode) {
+    try {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        //这种方案适用于 API 26, 即8.0（含8.0）以上可以用
+        ApplicationInfo appInfo = activity.getApplicationInfo();
+        String pkg = activity.getApplicationContext().getPackageName();
+        int uid = appInfo.uid;
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+        intent.putExtra(Settings.EXTRA_APP_PACKAGE, pkg);
+        intent.putExtra(Settings.EXTRA_CHANNEL_ID, uid);
+        activity.startActivityForResult(intent, requestCode);
+      } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        //这种方案适用于 API21——25，即 5.0——7.1 之间的版本可以使用
+        ApplicationInfo appInfo = activity.getApplicationInfo();
+        String pkg = activity.getApplicationContext().getPackageName();
+        int uid = appInfo.uid;
+        Intent intent = new Intent();
+        intent.setAction("android.settings.APP_NOTIFICATION_SETTINGS");
+        intent.putExtra("app_package", pkg);
+        intent.putExtra("app_uid", uid);
+        activity.startActivityForResult(intent, requestCode);
+      } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT) {
+        ApplicationInfo appInfo = activity.getApplicationInfo();
+        String pkg = activity.getApplicationContext().getPackageName();
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setData(Uri.parse("package:" + pkg));
+        activity.startActivityForResult(intent, requestCode);
+      } else {
+        Intent intent = new Intent(Settings.ACTION_SETTINGS);
+        activity.startActivityForResult(intent, requestCode);
+      }
+    } catch (Exception e) {
+      Intent intent = new Intent(Settings.ACTION_SETTINGS);
+      activity.startActivityForResult(intent, requestCode);
+    }
+  }
+
+  /**
+   * 设置通知开关
+   */
+  public static void openNotificationSetting(Fragment fragment, int requestCode) {
     try {
       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
         //这种方案适用于 API 26, 即8.0（含8.0）以上可以用
@@ -185,7 +231,7 @@ public final class NotificationHelp {
         intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
         intent.putExtra(Settings.EXTRA_APP_PACKAGE, pkg);
         intent.putExtra(Settings.EXTRA_CHANNEL_ID, uid);
-        fragment.startActivityForResult(intent, 100);
+        fragment.startActivityForResult(intent, requestCode);
       } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
         //这种方案适用于 API21——25，即 5.0——7.1 之间的版本可以使用
         ApplicationInfo appInfo = fragment.getContext().getApplicationInfo();
@@ -195,7 +241,7 @@ public final class NotificationHelp {
         intent.setAction("android.settings.APP_NOTIFICATION_SETTINGS");
         intent.putExtra("app_package", pkg);
         intent.putExtra("app_uid", uid);
-        fragment.startActivityForResult(intent, 100);
+        fragment.startActivityForResult(intent, requestCode);
       } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT) {
         ApplicationInfo appInfo = fragment.getContext().getApplicationInfo();
         String pkg = fragment.getContext().getApplicationContext().getPackageName();
@@ -203,14 +249,19 @@ public final class NotificationHelp {
         intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
         intent.addCategory(Intent.CATEGORY_DEFAULT);
         intent.setData(Uri.parse("package:" + pkg));
-        fragment.startActivityForResult(intent, 100);
+        fragment.startActivityForResult(intent, requestCode);
       } else {
         Intent intent = new Intent(Settings.ACTION_SETTINGS);
-        fragment.startActivityForResult(intent, 100);
+        fragment.startActivityForResult(intent, requestCode);
       }
     } catch (Exception e) {
       Intent intent = new Intent(Settings.ACTION_SETTINGS);
-      fragment.startActivityForResult(intent, 100);
+      fragment.startActivityForResult(intent, requestCode);
     }
+  }
+
+  public interface IChannelSetting {
+
+    void onChannelSetting(NotificationChannel channel);
   }
 }
