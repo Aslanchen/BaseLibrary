a29bb726ab1894cb9d0b28d97e13505495ae3c34
chenhengfei <chenhengfei@yy.com>
2020-06-10 14:39:36 +0800

    fix

--- baselibrary/src/main/java/com/aslan/baselibrary/http/NetManager.java
+++ baselibrary/src/main/java/com/aslan/baselibrary/http/NetManager.java
@@ -4,6 +4,18 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+import com.aslan.baselibrary.utils.LogUtils;
+
+import java.util.concurrent.TimeUnit;
+
+import androidx.annotation.NonNull;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * 网络基类
  *
@@ -12,43 +24,70 @@ import android.net.NetworkInfo;
  */
 public final class NetManager {
 
-  /**
-   * 网络是否可用
-   */
-  public static boolean isNetworkAvailable(Context context) {
-    ConnectivityManager connectivityManager = (ConnectivityManager) context
-        .getSystemService(Context.CONNECTIVITY_SERVICE);
+    /**
+     * 网络是否可用
+     */
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
 
-    if (null == connectivityManager) {
-      return false;
-    } else {
-      NetworkInfo[] networkInfos = connectivityManager.getAllNetworkInfo();
-      if (networkInfos == null) {
-        return false;
-      } else {
-        for (int i = 0; i < networkInfos.length; i++) {
-          if (NetworkInfo.State.CONNECTED == networkInfos[i].getState()) {
-            return true;
-          }
+        if (null == connectivityManager) {
+            return false;
+        } else {
+            NetworkInfo[] networkInfos = connectivityManager.getAllNetworkInfo();
+            if (networkInfos == null) {
+                return false;
+            } else {
+                for (int i = 0; i < networkInfos.length; i++) {
+                    if (NetworkInfo.State.CONNECTED == networkInfos[i].getState()) {
+                        return true;
+                    }
+                }
+            }
         }
-      }
+
+        return false;
     }
 
-    return false;
-  }
+    /**
+     * 是否为wifi网络
+     */
+    public final boolean isWifiConnection(Context context) {
+        final ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (null == connectivityManager) {
+            return false;
+        }
 
-  /**
-   * 是否为wifi网络
-   */
-  public final boolean isWifiConnection(Context context) {
-    final ConnectivityManager connectivityManager = (ConnectivityManager) context
-        .getSystemService(Context.CONNECTIVITY_SERVICE);
-    if (null == connectivityManager) {
-      return false;
+        final NetworkInfo networkInfo = connectivityManager
+                .getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        return (networkInfo != null && networkInfo.isConnectedOrConnecting());
     }
 
-    final NetworkInfo networkInfo = connectivityManager
-        .getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-    return (networkInfo != null && networkInfo.isConnectedOrConnecting());
-  }
+    public static final String TAG_LOG = "OkHttp";
+
+    public static <T> T create(@NonNull String url, Class<T> service) {
+        HttpLoggingInterceptor.Logger logger = new HttpLoggingInterceptor.Logger() {
+            @Override
+            public void log(String message) {
+                LogUtils.d(TAG_LOG, message);
+            }
+        };
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor(logger);
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(10, TimeUnit.SECONDS)
+                .addInterceptor(logging)
+                .build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(url)
+                .client(okHttpClient)
+                .addCallAdapterFactory(
+                        RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit.create(service);
+    }
 }
