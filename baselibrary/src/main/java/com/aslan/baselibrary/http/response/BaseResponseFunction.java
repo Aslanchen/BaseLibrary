73804330182deb963eda85901b8d4819950634d6
chenhengfei <chenhengfei@yy.com>
2020-07-24 22:59:48 +0800

    增加token异常处理

--- baselibrary/src/main/java/com/aslan/baselibrary/http/response/BaseResponseFunction.java
+++ baselibrary/src/main/java/com/aslan/baselibrary/http/response/BaseResponseFunction.java
@@ -1,14 +1,14 @@
 package com.aslan.baselibrary.http.response;
 
 import android.content.Context;
-
-import com.aslan.baselibrary.http.BaseError;
-import com.aslan.baselibrary.http.IHttpBean;
-import com.aslan.baselibrary.http.NetManager;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import com.aslan.baselibrary.http.BaseError;
+import com.aslan.baselibrary.http.EventTokenError;
+import com.aslan.baselibrary.http.IHttpBean;
+import com.aslan.baselibrary.http.NetManager;
 import io.reactivex.functions.Function;
+import org.greenrobot.eventbus.EventBus;
 
 /**
  * 处理业务异常
@@ -18,27 +18,28 @@ import io.reactivex.functions.Function;
  */
 abstract class BaseResponseFunction<T, R> implements Function<IHttpBean<T>, R> {
 
-    private Context context;
+  private Context context;
 
-    public BaseResponseFunction(Context context) {
-        this.context = context;
+  public BaseResponseFunction(Context context) {
+    this.context = context;
+  }
+
+  @Override
+  public R apply(IHttpBean<T> respone) throws Exception {
+    if (respone.isTokenError()) {
+      EventBus.getDefault().post(new EventTokenError(respone.getCode()));
+      return error(new BaseError(NetManager.ERROR_TOKEN_ERROR,
+          context.getString(com.aslan.baselibrary.R.string.error_net_token_error)));
     }
 
-    @Override
-    public R apply(IHttpBean<T> respone) throws Exception {
-        if (respone.isTokenError()) {
-            return error(new BaseError(NetManager.ERROR_TOKEN_ERROR,
-                    context.getString(com.aslan.baselibrary.R.string.error_net_token_error)));
-        }
-
-        if (respone.isSuccessful()) {
-            return handleData(respone.getData());
-        } else {
-            return error(new BaseError(respone.getCode(), respone.getMessage()));
-        }
+    if (respone.isSuccessful()) {
+      return handleData(respone.getData());
+    } else {
+      return error(new BaseError(respone.getCode(), respone.getMessage()));
     }
+  }
 
-    abstract R error(@NonNull BaseError ex);
+  abstract R error(@NonNull BaseError ex);
 
-    abstract R handleData(@Nullable T item);
+  abstract R handleData(@Nullable T item);
 }
