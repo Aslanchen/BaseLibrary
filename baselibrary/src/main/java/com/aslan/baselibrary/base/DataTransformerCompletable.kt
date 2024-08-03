f8a4b35fdd624e9161526ec97a0f7bae4af4c0c2
Aslanchen <122560007@163.com>
2024-08-03 11:45:25 +0800

    等待框重构

--- baselibrary/src/main/java/com/aslan/baselibrary/base/DataTransformerCompletable.kt
+++ baselibrary/src/main/java/com/aslan/baselibrary/base/DataTransformerCompletable.kt
@@ -1,6 +1,9 @@
 package com.aslan.baselibrary.base
 
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.aslan.baselibrary.R
 import com.aslan.baselibrary.exception.TokenException
 import com.aslan.baselibrary.listener.IBaseView
@@ -34,14 +37,32 @@ open class DataTransformerCompletable(
 ) :
     CompletableTransformer {
 
+    private var progressDialog: WaitingDialog? = null
+
+    private fun getFragmentManager(): FragmentManager? {
+        if (mBaseView is Fragment) {
+            return mBaseView.parentFragmentManager
+        } else if (mBaseView is AppCompatActivity) {
+            return mBaseView.supportFragmentManager
+        }
+        return null
+    }
+
     private fun doOnSubscribe() {
         if (isShowProgressbar) {
-            if (progressbarMsg != null) {
-                mBaseView.showProgressBar(progressbarCanCancel, progressbarMsg)
-            } else if (progressbarMsgResId != null) {
-                mBaseView.showProgressBar(progressbarCanCancel, progressbarMsgResId)
-            } else {
-                mBaseView.showProgressBar(progressbarCanCancel)
+            val manager = getFragmentManager()
+            if (manager != null) {
+                val waitingDialogBuilder = WaitingDialog.Builder(mBaseView.requireContext())
+                waitingDialogBuilder.setCancelable(progressbarCanCancel)
+                if (progressbarMsgResId != null) {
+                    waitingDialogBuilder.setMessage(progressbarMsgResId)
+                }
+
+                if (progressbarMsg != null) {
+                    waitingDialogBuilder.setMessage(progressbarMsg)
+                }
+
+                this.progressDialog = waitingDialogBuilder.show(manager)
             }
         }
 
@@ -50,7 +71,11 @@ open class DataTransformerCompletable(
 
     private fun doOnError(error: Throwable) {
         if (isShowProgressbar) {
-            mBaseView.closeProgressBar()
+            try {
+                progressDialog?.dismiss()
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+            }
         }
         clickView?.isEnabled = true
 
@@ -75,7 +100,11 @@ open class DataTransformerCompletable(
 
     private fun doFinally() {
         if (isShowProgressbar) {
-            mBaseView.closeProgressBar()
+            try {
+                progressDialog?.dismiss()
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+            }
         }
         clickView?.isEnabled = true
     }
--- baselibrary/src/main/java/com/aslan/baselibrary/base/WaitingDialog.kt
+++ baselibrary/src/main/java/com/aslan/baselibrary/base/WaitingDialog.kt
@@ -1,5 +1,6 @@
 package com.aslan.baselibrary.base
 
+import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
@@ -7,6 +8,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.StringRes
+import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
@@ -16,11 +19,7 @@ import com.aslan.baselibrary.R
  * 等待框
  */
 open class WaitingDialog : DialogFragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         return inflater.inflate(R.layout.layout_wait_dialog, container)
     }
@@ -34,27 +33,55 @@ open class WaitingDialog : DialogFragment() {
 
     open fun iniView(view: View) {
         var msg = arguments?.getString("msg")
-        val isShownMsg = arguments?.getBoolean("isShownMsg") ?: true
-        if (msg.isNullOrEmpty()) {
+        if (msg == null) {
             msg = getString(R.string.progress_waiting)
         }
 
+        if (msg.isBlank()) {
+            view.findViewById<TextView>(R.id.tvText).isGone = true
+        } else {
+            view.findViewById<TextView>(R.id.tvText).isVisible = true
+        }
         view.findViewById<TextView>(R.id.tvText).setText(msg)
-        view.findViewById<TextView>(R.id.tvText).isVisible = isShownMsg
     }
 
     open fun iniListener() {}
 
-    open fun iniData() {
-    }
+    open fun iniData() {}
 
-    open fun show(manager: FragmentManager, msg: String? = null, isShownMsg: Boolean = true) {
+    open fun show(manager: FragmentManager, builder: Builder) {
         val bundle = Bundle()
-        if (msg != null) {
-            bundle.putString("msg", msg)
+        if (builder.msg != null) {
+            bundle.putString("msg", builder.msg.toString())
         }
-        bundle.putBoolean("isShownMsg", isShownMsg)
         arguments = bundle
         super.show(manager, WaitingDialog::class.java.simpleName)
+    }
+
+    data class Builder(val mContext: Context) {
+        var cancelable: Boolean? = null
+        var msg: CharSequence? = null
+
+        fun setCancelable(cancelable: Boolean): Builder {
+            this.cancelable = cancelable
+            return this
+        }
+
+        fun setMessage(msg: String): Builder {
+            this.msg = msg
+            return this
+        }
+
+        fun setMessage(@StringRes resId: Int): Builder {
+            msg = mContext.getText(resId)
+            return this
+        }
+
+        fun show(manager: FragmentManager): WaitingDialog {
+            val dialog = WaitingDialog()
+            dialog.isCancelable = (cancelable ?: true)
+            dialog.show(manager, this)
+            return dialog
+        }
     }
 }
